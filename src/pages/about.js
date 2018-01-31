ffbc4ed60ca11b0591d6843be9e47e0295574484
joncancode <jc.haines19@gmail.com>
2018-01-30 23:14:48 -0500

    test revert

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,7 +14,7 @@ class About extends React.Component {
           >
             <h1 className="page-title">About Me</h1>
 
-            <p>TEST Hi, I'm
+            <p>Hi, I'm
                 <strong> Jonathan Haines</strong>, a developer from
                 <strong> Brooklyn</strong>,
                 <strong> New York</strong>.
