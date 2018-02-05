3a9c413280d31dff17939c501e687f571f25e9fe
joncancode <jc.haines19@gmail.com>
2018-02-04 22:08:18 -0500

    add projects to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,10 +3,39 @@ import Link from 'gatsby-link'
 
 const Projects = () => (
   <div 
-  style={{height: '60vh'}}
+  style={{height: '80vh'}}
   >
-    <h1 className="page-title">Hi from the project page</h1>
-    <p>Under construction.</p>
+    <h1 className="page-title">What I've been working on...</h1>
+    <div>
+    <h4>When Again?</h4>
+    <div style={{paddingBottom: '20px'}}>
+    <button><a target="_new" href="https://github.com/joncancode/remind-me/">Github</a></button>
+    <button><a target="_new" href="https://when-did-that-happen.herokuapp.com/">Demo</a></button>
+    </div>
+    </div>
+    <div>
+    <h4>Pokemon Python</h4>
+    <div style={{paddingBottom: '20px'}}>
+    <button><a target="_new" href="https://github.com/joncancode/console-adventure/">Github</a></button>
+    <button><a target="_new" href="https://github.com/joncancode/console-adventure#try-it-yourself">Demo it yourself</a></button>
+    </div>
+    </div>
+    <div>
+    <h4>Note Hero</h4>
+    <div style={{paddingBottom: '20px'}}>
+    <button><a target="_new" href="https://github.com/joncancode/flashcards/">Github</a></button>
+    <button><a target="_new" href="https://notehero.herokuapp.com/">Demo</a></button>
+    </div>
+    </div>
+    <div>
+    <h4>My Rex</h4>
+    <div style={{paddingBottom: '20px'}}>
+    <button><a target="_new" href="https://github.com/joncancode/my-rex/">Github</a></button>
+    <button><a target="_new" href="https://myrex.herokuapp.com/">Demo</a></button>
+    </div>
+    </div>
+
+
     <Link style={{marginTop: '40px'}} to="/">Go home</Link>
   </div>
 )
