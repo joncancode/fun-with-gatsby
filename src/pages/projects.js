c3e8d9a2d2c88d81a1472909ed6a5a4d045a5c55
joncancode <jc.haines19@gmail.com>
2018-02-05 00:03:55 -0500

    under border to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,29 +6,29 @@ const Projects = () => (
   style={{height: '80vh'}}
   >
     <h1 className="page-title">What I've been working on...</h1>
-    <div>
-    <h4>When Again?</h4>
+    <div style={{borderBottom: '1px solid #ff7417'}}>
+    <h4 style={{paddingTop: '10px'}}>When Again?</h4>
     <div style={{paddingBottom: '20px'}}>
     <button><a target="_new" href="https://github.com/joncancode/remind-me/">Github</a></button>
     <button><a target="_new" href="https://when-did-that-happen.herokuapp.com/">Demo</a></button>
     </div>
     </div>
-    <div>
-    <h4>Pokemon Python</h4>
+    <div style={{borderBottom: '1px solid #ff7417'}}>
+    <h4 style={{paddingTop: '10px'}}>Pokemon Python</h4>
     <div style={{paddingBottom: '20px'}}>
     <button><a target="_new" href="https://github.com/joncancode/console-adventure/">Github</a></button>
     <button><a target="_new" href="https://github.com/joncancode/console-adventure#try-it-yourself">Demo it yourself</a></button>
     </div>
     </div>
-    <div>
-    <h4>Note Hero</h4>
+    <div style={{borderBottom: '1px solid #ff7417'}}>
+    <h4 style={{paddingTop: '10px'}}>Note Hero</h4>
     <div style={{paddingBottom: '20px'}}>
     <button><a target="_new" href="https://github.com/joncancode/flashcards/">Github</a></button>
     <button><a target="_new" href="https://notehero.herokuapp.com/">Demo</a></button>
     </div>
     </div>
     <div>
-    <h4>My Rex</h4>
+    <h4 style={{paddingTop: '10px'}}>My Rex</h4>
     <div style={{paddingBottom: '20px'}}>
     <button><a target="_new" href="https://github.com/joncancode/my-rex/">Github</a></button>
     <button><a target="_new" href="https://myrex.herokuapp.com/">Demo</a></button>
