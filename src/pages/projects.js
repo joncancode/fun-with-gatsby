55be79c5cd6b7f77903a34bf36cafd3aa7edb463
joncancode <jc.haines19@gmail.com>
2018-02-06 19:50:33 -0500

    center projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,35 +3,39 @@ import Link from 'gatsby-link'
 
 const Projects = () => (
   <div 
-  style={{height: '80vh'}}
+  style={{height: '110vh'}}
   >
     <h1 className="page-title">What I've been working on...</h1>
-    <div style={{borderBottom: '1px solid #ff7417'}}>
+    <div style={{textAlign: 'center'}}>
     <h4 style={{paddingTop: '10px'}}>When Again?</h4>
-    <div style={{paddingBottom: '20px'}}>
+    <div style={{paddingBottom: '5px'}}>
     <button><a target="_new" href="https://github.com/joncancode/remind-me/">Github</a></button>
     <button><a target="_new" href="https://when-did-that-happen.herokuapp.com/">Demo</a></button>
+    <p style={{color: "#ff7417"}}>____________</p>
     </div>
     </div>
-    <div style={{borderBottom: '1px solid #ff7417'}}>
+    <div style={{textAlign: 'center'}}>
     <h4 style={{paddingTop: '10px'}}>Pokemon Python</h4>
-    <div style={{paddingBottom: '20px'}}>
+    <div style={{paddingBottom: '5px'}}>
     <button><a target="_new" href="https://github.com/joncancode/console-adventure/">Github</a></button>
     <button><a target="_new" href="https://github.com/joncancode/console-adventure#try-it-yourself">Demo it yourself</a></button>
+    <p style={{color: "#ff7417"}}>____________</p>
     </div>
     </div>
-    <div style={{borderBottom: '1px solid #ff7417'}}>
+    <div style={{textAlign: 'center'}}>
     <h4 style={{paddingTop: '10px'}}>Note Hero</h4>
-    <div style={{paddingBottom: '20px'}}>
+    <div style={{paddingBottom: '5px'}}>
     <button><a target="_new" href="https://github.com/joncancode/flashcards/">Github</a></button>
     <button><a target="_new" href="https://notehero.herokuapp.com/">Demo</a></button>
+    <p style={{color: "#ff7417"}}>____________</p>
     </div>
     </div>
-    <div>
+    <div style={{textAlign: 'center'}}>
     <h4 style={{paddingTop: '10px'}}>My Rex</h4>
-    <div style={{paddingBottom: '20px'}}>
+    <div style={{paddingBottom: '5px'}}>
     <button><a target="_new" href="https://github.com/joncancode/my-rex/">Github</a></button>
     <button><a target="_new" href="https://myrex.herokuapp.com/">Demo</a></button>
+    <p style={{color: "#ff7417"}}>____________</p>
     </div>
     </div>
 
