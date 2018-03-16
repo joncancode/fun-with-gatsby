09e817fb615a1be1f0bb721b6c3c19ef3b83aa12
joncancode <jc.haines19@gmail.com>
2018-03-16 18:01:30 -0400

    update links to res

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,8 +27,8 @@ class About extends React.Component {
                 <strong> jc.haines19@gmail.com</strong>.
                 </p>
                 <p> My resume/CV can be found
-                <a style={{color: '#ff7417'}} class="about-text-links" target="_new" href="https://drive.google.com/file/d/1Q6GNKX-xxj04WdHd18sK3sBBHvOZP8fM/view?usp=sharing"> here </a>(
-                <a style={{color: '#ff7417'}} class="about-text-links" target="_new" href="https://drive.google.com/file/d/11cbgqy5bvnwnroCIubBJ936o7JQPdgl0/view?usp=sharing"> or here to save ink</a> ).
+                <a style={{color: '#ff7417'}} class="about-text-links" target="_new" href="https://drive.google.com/file/d/1ineWI7GTVz3UkPN1WEp5EeWZLsConjsu/view?usp=sharing"> here </a>(
+                <a style={{color: '#ff7417'}} class="about-text-links" target="_new" href="https://drive.google.com/file/d/18mEckJSyKsAj4eEwjntj2dEGjFKmYOi2/view?usp=sharing"> or here to save ink</a> ).
                 </p>
                 <div style={{marginTop: '40px'}}>
                 <Link className="page-links" to="/">Go home</Link>
